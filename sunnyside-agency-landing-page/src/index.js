269398bbd79955bd5b46875cb25b4a9067d0d679
T. Safa Elmalı <tsafaelmali@gmail.com>
2021-06-14 16:06:51 +0300

    Initialize design files
    Closes #3

--- sunnyside-agency-landing-page/src/index.js
+++ sunnyside-agency-landing-page/src/index.js
@@ -1,7 +1,15 @@
-import "styles/index.css";
+import GlobalStyle from "styles/GlobalStyle";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "App/App";
+import theme from "styles/theme";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
---
+++ sunnyside-agency-landing-page/src/styles/theme.js
@@ -0,0 +1,24 @@
+const theme = {
+  colors: {
+    primary: {
+      softRed: "hsl(7, 99%, 70%)",
+      yellow: "hsl(51, 100%, 49%)",
+      darkDesaturatedCyan: "hsl(167, 40%, 24%)",
+      darkBlue: "hsl(198, 62%, 26%)",
+      darkModerateCyan: "hsl(168, 34%, 41%)",
+    },
+    neutral: {
+      veryDarkDesaturatedBlue: "hsl(212, 27%, 19%)",
+      veryDarkGrayishBlue: "hsl(213, 9%, 39%)",
+      darkGrayishBlue: "hsl(232, 10%, 55%)",
+      grayishBlue: "hsl(210, 4%, 67%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+  },
+  typography: {
+    barlow: "'Barlow',sans-serif",
+    fraunces: "'Fraunces', sans-serif",
+  },
+};
+
+export default theme;
